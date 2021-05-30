74b85b0f660e7ccf62ebfaadb072e864eeb31641
hyunjiyoo <solteee23@gmail.com>
2021-05-30 19:28:51 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by USER on 2021/05/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
