a18e47c6d4b99dafd1311cbc0512531ec09f478d
hyunjiyoo <solteee23@gmail.com>
2021-05-31 15:39:56 +0900

    예산

--- algorithm/budget/budget.cpp
+++ algorithm/budget/budget.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+int N, M;
+std::vector<int> budget;
+
+void input() {
+    std::scanf("%d", &N);
+    budget.resize(N);
+    
+    for (int i = 0; i < N; ++i) {
+        std::scanf("%d", &budget[i]);
+    }
+    std::scanf("%d", &M);
+}
+
+int solve() {
+    std::sort(budget.begin(), budget.end());
+    
+    int low = 0;
+    int high = budget[N - 1];
+    int mid = budget[N - 1] / 2;
+    int sum;
+    
+    while (high >= low) {
+        sum = 0;
+        
+        for (int i = 0; i < N; ++i) {
+            sum += (mid < budget[i]) ? mid : budget[i];
+        }
+        
+        if (sum == M) return mid;
+        
+        if (sum < M)
+            low = mid + 1;
+        else
+            high = mid - 1;
+        
+        mid = (low + high) / 2;
+    }
+    
+    return mid;
+}
 
 int main (int argc, const char * argv []) {
+
+    input();
+    std::printf("%d", solve());
     
     return 0;
 }
