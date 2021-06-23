198b33b8bdf9c86bbbacc79b1eb6fbbf34c66b75
hyunjiyoo <solteee23@gmail.com>
2021-06-23 18:48:10 +0900

    게임 맵 최단거리 (bfs)

--- algorithm/gamemap/gamemap.cpp
+++ algorithm/gamemap/gamemap.cpp
@@ -1,68 +1,64 @@
 #include <vector>
-#include <limits>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
-vector<vector<bool>> reachable;
-int min_count = numeric_limits<int>::max();
-int offset[4][2] = {{ 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 }};
-bool flag = false;
-
-void play(vector<vector<int>>& maps, int y, int x, int count) {
-    
-    if (count > min_count) return;
-    
-    if (y == (int)maps.size() - 1 && x == (int)maps[0].size() - 1) {
-        min_count = (min_count < count) ? min_count : count;
-        flag = true;
-        return;
-    }
-    
-    for (int i = 0; i < 4; ++i) {
-        int ny = y + offset[i][0];
-        int nx = x + offset[i][1];
-        
-        if (!(0 <= ny && ny < (int)maps.size() && 0 <= nx && nx < (int)maps[0].size()))
-            continue;
-        
-        if (reachable[ny][nx]) continue;
-        
-        if (maps[ny][nx]) {
-            reachable[ny][nx] = true;
-        
-            play(maps, ny, nx, count + 1);
-            reachable[ny][nx] = false;
-        }
-        
-    }
-    
-}
-
 int solution(vector<vector<int>> maps) {
-        
+    int count = -1;
+    bool flag = false;
+    queue<pair<int, int>> q;
+    vector<vector<bool>> reachable;
+    int offset[4][2] = {{ 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 }};
+    
     reachable.resize(maps.size());
     for (int i = 0; i < reachable.size(); ++i)
-        reachable[i].resize(maps[i].size());
+        reachable[i].resize(maps[0].size());
     
     
-    play(maps,0, 0, 1);
+    int y, x;
+    y = x = 0;
+    q.push(pair<int, int>(y, x));
     
-    if (!flag)
-        min_count = -1;
-
-    return min_count;
+    while (!q.empty()) {
+        y = q.front().first;
+        x = q.front().second;
+        
+        if (y == maps.size() - 1 && x == maps[0].size() - 1) {
+            flag = true;
+            break;
+        }
+        
+        for (int i = 0; i < 4; ++i) {
+            int ny = y + offset[i][0];
+            int nx = x + offset[i][1];
+            
+            if (!(0 <= ny && ny < maps.size() && 0 <= nx && nx < maps[0].size()))
+                continue;
+           
+            if (!reachable[ny][nx] && maps[ny][nx]) {
+                reachable[y][x] = true;
+                q.push(pair<int, int>(ny, nx));
+                maps[ny][nx] = maps[y][x] + 1;
+            }
+        }
+        
+        q.pop();
+    }
+    
+    if (flag)
+        count = maps[y][x];
+    
+    return count;
 }
 
 
 int main() {
-    
     vector<vector<int>> maps = {{1,0,1,1,1},
                                 {1,0,1,0,1},
                                 {1,0,1,1,1},
-                                {1,1,1,0,0},
+                                {1,1,1,0,1},
                                 {0,0,0,0,1}};
-    
     
     cout << solution(maps) << endl;
     
