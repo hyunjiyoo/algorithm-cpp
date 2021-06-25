0ec1e0a0072a5d302d3690ceaca7f7d7a5552967
hyunjiyoo <solteee23@gmail.com>
2021-06-25 21:44:41 +0900

    z

---
+++ algorithm/codingtest1/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+void solution() {
+    
+    
+}
+
+int main (int argc, const char * argv []) {
+        
+    vector<int> v;
+    
+    cout << solution() << endl;
+    
+    return 0;
+}
