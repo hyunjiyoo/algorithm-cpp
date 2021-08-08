8075852204468eec326fefd3fcb1d1217ff9ecfd
hyunjiyoo <solteee23@gmail.com>
2021-08-08 17:14:47 +0900

    다리위를 지나는 트럭

--- algorithm/bridgetruck/bridgetruck.cpp
+++ algorithm/bridgetruck/bridgetruck.cpp
@@ -1,51 +1,53 @@
 #include <vector>
-#include <iostream>
 #include <queue>
 
 using namespace std;
 
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int seq = 1;
-    queue<pair<int, int>> truck;
-    vector<pair<int, int>> pass_truck;
-    
-    for (int i = 0; i < truck_weights.size(); ++i)
-        truck.push({ truck_weights[i], 1 });
 
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 1;
     
-    int w = truck.front().first;
-    pass_truck.push(truck.front());
-    truck.pop();
+    vector<int> v;
+    queue<int> q;
+
+    v.resize(truck_weights.size());
+    fill_n(v.begin(), v.size(), 0);
+    q.push(truck_weights[0]);
     
-    while (!pass_truck.empty()) {
-        
-        if (pass_truck.front().second < bridge_length) {
-            
-            ++pass_truck.front().second;
-        }
-        else {
-            w -= pass_truck.front().first;
-            pass_truck.pop();
-        }
-        
-        if (!truck.empty() && w + truck.front().first <= weight) {
-            pass_truck.push(truck.front());
-            w += truck.front().first;
-            truck.pop();
+    v[0] = 1;
+    
+    int w = q.front();
+    int next = 1;
+    int prev = 0;
+    
+    while (!q.empty()) {
+        if (v[prev] == bridge_length) {
+            ++prev;
+            w -= q.front();
+            q.pop();
         }
         
-        ++seq;
+        if (w + truck_weights[next] <= weight && next < truck_weights.size()) {
+            w += truck_weights[next];
+            q.push(truck_weights[next++]);
+        }
+        
+        for (int i = prev; i < next; ++i)
+            ++v[i];
+        
+        ++answer;
     }
     
-    return seq;
+    return answer;
 }
+
 
 int main() {
     
-    cout << solution(3, 15, {7,4,5,6}) << endl;
-    cout << solution(2, 10, {7,4,5,6}) << endl;
-    cout << solution(100, 100, {10}) << endl;
-    cout << solution(100, 100, {10,10,10,10,10,10,10,10,10,10}) << endl;
+    cout << solution(3, 11, {7,4,5,6}) << endl; // 8
+    cout << solution(2, 10, {7,4,5,6}) << endl; // 8
+    cout << solution(100, 100, {10}) << endl; // 101
+    cout << solution(100, 100, {10,10,10,10,10,10,10,10,10,10}) << endl; // 110
     
     return 0;
 }
